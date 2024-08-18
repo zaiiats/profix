c7762e29a72258e2938c34fa14cdb03c399bbf6c
zaiiats <zaiatsoleksandry@gmail.com>
2024-08-18 14:25:29 +0300

    big problems with parcel

--- js/controler.js
+++ js/controler.js
@@ -1,13 +1,13 @@
 import HamburgerFunctionality from "./views/hamburgerView.js";
-import MapView from "./views/mapView.js";
+/*import MapView from "./views/mapView.js";*/
 import NavLinkFunctionality from "./views/navView.js";
 import PageInitialisation from "./views/pageInitView.js";
 import SlideshowView from "./views/slideshowView.js";
 import TextWriter from "./views/textWriterView.js";
 import ThemeSwitchFunctionalityView from "./views/themeSwitchFuncView.js";
 
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
+/*import L from "leaflet";
+import "leaflet/dist/leaflet.css";*/
 
 const startPage1 = async function () {
   const pageInit = new PageInitialisation();
@@ -18,7 +18,7 @@ const startPage = async function () {
   try {
     console.log(L);
 
-    const mapView = new MapView(); 
+    /*const mapView = new MapView(); */
     const hamburgerFunc = new HamburgerFunctionality();
     const navLinkFunc = new NavLinkFunctionality();
     const slideshowFunc = new SlideshowView();
