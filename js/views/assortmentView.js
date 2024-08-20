d450295c85902b70894cbd260698d767bb9eb459
zaiiats <zaiatsoleksandry@gmail.com>
2024-08-20 23:59:08 +0300

    adding assortment2.0

--- js/views/assortmentView.js
+++ js/views/assortmentView.js
@@ -1,14 +1,18 @@
 class AssortmentView {
   #carousel;
-  #prev;
-  #next;
+  #currentIndex;
+  #timer;
+  #regularWidth;
+  #activeWidth;
+  #fontSize;
   constructor() {
     this.#carousel = document.querySelector(".carousel");
     this.slides = Array.from(document.querySelectorAll(".card__slide"));
-    this.#prev = document.querySelector(".slideshow-wrapper__arrow--previous");
-    this.#next = document.querySelector(".slideshow-wrapper__arrow--next");
-    this.currentIndex = 0;
-    this.timer;
+    this.#currentIndex = 0;
+    this.#timer;
+    this.#regularWidth;
+    this.#activeWidth;
+    this.#fontSize;
 
     this.#initAssortment();
   }
@@ -58,40 +62,71 @@ class AssortmentView {
   // Carousel
 
   #initAssortment() {
+    this.#getWidth();
+    this.#addSwipeListener();
+    window.addEventListener("resize", this.#getWidth.bind(this));
+
     this.#carousel.addEventListener("click", this.#handleClick.bind(this));
     this.slides.forEach((slide) => {
-      slide.addEventListener("mouseenter", function() {
-        clearInterval(this.timer);
-      }.bind(this));
+      slide.style.setProperty(
+        "transform",
+        `translateX(-${this.#activeWidth / 2}px)`
+      );
+      slide.addEventListener(
+        "mouseenter",
+        function () {
+          clearInterval(this.#timer);
+        }.bind(this)
+      );
       slide.addEventListener("mouseleave", this.restartTimer.bind(this));
     });
+    this.restartTimer();
+  }
+
+  #getWidth() {
+    this.slides.map((slide) => {
+      slide.classList.contains("card__slide-active")
+        ? (this.#activeWidth = slide.getBoundingClientRect().width)
+        : (this.#regularWidth = slide.getBoundingClientRect().width);
+    });
+    this.#fontSize = parseFloat(
+      getComputedStyle(document.documentElement).fontSize
+    );
     this.restartTimer();
   }
 
   #handleClick(e) {
     if (
       this.slides.includes(e.target.closest(".card__slide")) &
-      !e.target.closest(".card__slide")?.classList.contains("card__slide-active")
+      !e.target
+        .closest(".card__slide")
+        ?.classList.contains("card__slide-active")
     ) {
-      const clickedIndex = this.slides.indexOf(e.target.closest(".card__slide"));
+      const clickedIndex = this.slides.indexOf(
+        e.target.closest(".card__slide")
+      );
       this.moveToSlide(clickedIndex);
-      this.currentIndex = clickedIndex;
+      this.#currentIndex = clickedIndex;
       this.restartTimer();
     }
-    if (e.target.closest(".slideshow-wrapper__arrow--previous")) () => {
+    if (e.target.closest(".slideshow-wrapper__arrow--previous")) {
       this.moveToPrevSlide();
       this.restartTimer();
     }
-    if (e.target.closest(".slideshow-wrapper__arrow--next")) () => {
+    if (e.target.closest(".slideshow-wrapper__arrow--next")) {
       this.moveToNextSlide();
       this.restartTimer();
     }
   }
 
   moveToSlide(index) {
-    this.slides.forEach((slide) => slide.classList.remove("card__slide-active"));
+    this.slides.forEach((slide) =>
+      slide.classList.remove("card__slide-active")
+    );
     this.slides[index].classList.add("card__slide-active");
-    const transformValue = `translateX(-${index * 17 + 10}rem)`;
+    const transformValue = `translateX(-${
+      index * (this.#regularWidth + 2 * this.#fontSize) + this.#activeWidth / 2
+    }px)`;
     this.slides.forEach((slide) =>
       slide.style.setProperty("transform", transformValue)
     );
@@ -99,21 +134,42 @@ class AssortmentView {
   }
 
   moveToNextSlide() {
-    this.currentIndex = (this.currentIndex + 1) % this.slides.length;
-    this.moveToSlide(this.currentIndex);
+    this.#currentIndex = (this.#currentIndex + 1) % this.slides.length;
+    this.moveToSlide(this.#currentIndex);
     this.restartTimer();
   }
 
   moveToPrevSlide() {
-    this.currentIndex =
-      (this.currentIndex - 1 + this.slides.length) % this.slides.length;
-    this.moveToSlide(this.currentIndex);
+    this.#currentIndex =
+      (this.#currentIndex - 1 + this.slides.length) % this.slides.length;
+    this.moveToSlide(this.#currentIndex);
     this.restartTimer();
   }
 
   restartTimer() {
-    clearInterval(this.timer);
-    this.timer = setInterval(this.moveToNextSlide.bind(this), 4000);
+    clearInterval(this.#timer);
+    this.#timer = setInterval(this.moveToNextSlide.bind(this), 5000);
+  }
+  #addSwipeListener() {
+    let touchstartX = 0;
+    let touchendX = 0;
+
+    this.#carousel.addEventListener("touchstart", (event) => {
+      touchstartX = event.changedTouches[0].screenX;
+    });
+
+    this.#carousel.addEventListener("touchend", (event) => {
+      touchendX = event.changedTouches[0].screenX;
+      this.#handleSwipeGesture(touchstartX, touchendX);
+    });
+  }
+  #handleSwipeGesture(startX, endX) {
+    const swipeThreshold = 20;
+    if (startX - endX > swipeThreshold) {
+      this.moveToNextSlide();
+    } else if (endX - startX > swipeThreshold) {
+      this.moveToPrevSlide();
+    }
   }
 }
 
