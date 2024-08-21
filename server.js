3c67cb8620af1f5f51fa114a9b45fa528427be8c
zaiiats <zaiatsoleksandry@gmail.com>
2024-08-21 12:35:44 +0300

    adding assortment 3.2

--- server.js
+++ server.js
@@ -4,14 +4,9 @@ import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Use CORS with specific origin(s) allowed
-app.use(cors({
-  origin: "*"  // Allow all origins (for testing purposes, but you might want to specify your domain)
-}));
+app.use(cors());
+app.use(express.json());
 
-app.use(json());
-
-// Sample data
 const data = [
   { id: 1, name: "Анкер однорозпірний", like: "yes", bookmark: "no" },
   { id: 2, name: "Анкер дворозпірний", like: "no", bookmark: "yes" },
@@ -21,12 +16,10 @@ const data = [
   { id: 6, name: "Гвинт", like: "yes", bookmark: "yes" },
 ];
 
-// GET endpoint to retrieve data
-app.get("/data", (req, res) => {
+app.get('/data', (req, res) => {
   res.json(data);
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
